a7379a67fb5c69295418d0f00783085e834f0aa7
yahyakansu <yahyakansusa@gmail.com>
2020-02-02 14:28:23 -0600

    add string introduction

---
+++ src/strings/Introduction.java
@@ -0,0 +1,18 @@
+package strings;
+
+import java.util.*;
+
+public class Introduction {
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        String A=scan.next();
+        String B=scan.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length()+B.length());
+        System.out.println(A.compareTo(B)>0?"Yes":"No");
+
+        System.out.println(A.substring(0,1).toUpperCase()+A.substring(1)+" "+B.substring(0,1).toUpperCase()+B.substring(1));
+
+    }
+}
