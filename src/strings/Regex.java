c4e687dcc7c8bdb95247cc1e09ad8980d2762805
yahyakansu <yahyakansusa@gmail.com>
2020-02-06 19:11:50 -0600

    add java regex

---
+++ src/strings/Regex.java
@@ -0,0 +1,24 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Regex {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            String IP = scan.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+/*
+     [01]?\\d{1,2}   matches numbers 0-199.
+     2[0-4]\\d       matches numbers 200-249
+     25[0-5]         matches numbers 250-255
+*/
+class MyRegex {
+    String num = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+    String pattern = num + "." +  num + "." +  num + "." + num;
+}
