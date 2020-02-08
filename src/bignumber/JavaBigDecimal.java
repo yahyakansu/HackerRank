5e35a15cf497f0f0efe89fcc9830440a3df0c7d4
yahyakansu <yahyakansusa@gmail.com>
2020-02-08 17:57:03 -0600

    add big decimal

---
+++ src/bignumber/JavaBigDecimal.java
@@ -0,0 +1,35 @@
+package bignumber;
+
+import java.math.*;
+import java.util.*;
+
+public class JavaBigDecimal {
+    public static void main(String []args){
+        //Input
+        Scanner scan= new Scanner(System.in);
+        int n=scan.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=scan.next();
+        }
+        scan.close();
+
+        //Write your code here
+        Comparator<String> customComparator = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                BigDecimal a = new BigDecimal(s1);
+                BigDecimal b = new BigDecimal(s2);
+                return b.compareTo(a); // descending order
+            }
+        };
+
+        Arrays.sort(s, 0, n, customComparator);
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
