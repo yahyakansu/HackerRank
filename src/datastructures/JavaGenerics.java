67afabfee7215768f2c1e0ed74b363bc0d4af081
yahyakansu <yahyakansusa@gmail.com>
2020-02-21 18:57:02 -0600

    add java generics

---
+++ src/datastructures/JavaGenerics.java
@@ -0,0 +1,44 @@
+package datastructures;
+
+import java.lang.reflect.Method;
+
+public class JavaGenerics {
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+
+    }
+}
+
+/*
+stout
+1
+2
+3
+Hello
+World
+ */
+
+class Printer
+{
+    //Write your code here
+
+    public <T> void printArray(T[] array){
+        for (T type:array) {
+            System.out.println(type);
+        }
+    }
+}
