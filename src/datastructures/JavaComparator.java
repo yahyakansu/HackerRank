01c3827fd254d85f61848865a34027f3b41faee7
yahyakansu <yahyakansusa@gmail.com>
2020-02-22 08:35:42 -0600

    add java comparator

---
+++ src/datastructures/JavaComparator.java
@@ -0,0 +1,61 @@
+package datastructures;
+
+import java.util.*;
+
+class Checker implements Comparator<Player>{
+    @Override
+    public int compare(Player p1, Player p2) {
+        if (p1.score==p2.score){
+            return p1.name.compareTo(p2.name);
+        }else {
+            return p2.score-p1.score;
+        }
+    }
+}
+
+class Player{
+    String name;
+    int score;
+
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+public class JavaComparator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
+
+/*
+Sample Input
+5
+amy 100
+david 100
+heraldo 50
+aakansha 75
+aleksa 150
+
+Sample Output
+aleksa 150
+amy 100
+david 100
+aakansha 75
+heraldo 50
+ */
